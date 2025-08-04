64a366954fff2e1d86c592e1c6fa53e91162cce0
layfredrc <frederic.lay@efrei.net>
2025-08-05 01:09:15 +0200

    google sign-in

--- app/login/action.ts
+++ app/login/action.ts
@@ -4,7 +4,7 @@ import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
 import { createClient } from '@/utils/supabase/server'
-import { cookies } from 'next/headers'
+import { cookies, headers } from 'next/headers'
 
 export async function login(formData: FormData) {
   const supabase = await createClient()
@@ -50,11 +50,13 @@ export async function signup(formData: FormData) {
 export async function signInWithOTP(formData: FormData) {
   const supabase = await createClient()
   const email = formData.get('email') as string
+  const nextHeaders = await headers()
+  const origin = nextHeaders.get('origin') ?? process.env.NEXT_PUBLIC_SITE_URL! // définis cette env en prod
 
   const { error } = await supabase.auth.signInWithOtp({
     email,
     options: {
-      emailRedirectTo: 'http://localhost:3000/auth/callback',
+      emailRedirectTo: `${origin}/auth/otp`,
     },
   })
 
@@ -71,5 +73,24 @@ export async function signInWithOTP(formData: FormData) {
   })
 
   revalidatePath('/', 'layout')
-  redirect('/auth/callback')
+  redirect('/auth/otp')
+}
+
+export async function signInWithGoogle() {
+  const supabase = await createClient()
+  const nextHeaders = await headers()
+  const origin = nextHeaders.get('origin') ?? process.env.NEXT_PUBLIC_SITE_URL! // définis cette env en prod
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      redirectTo: `${origin}/auth/callback`,
+      scopes: 'openid email profile',
+      // Quelques options utiles :
+      queryParams: { prompt: 'select_account', access_type: 'offline' },
+    },
+  })
+  if (error) {
+    redirect('/error')
+  }
+  redirect(data.url)
 }
